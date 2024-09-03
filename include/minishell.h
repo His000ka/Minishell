c18e4a0edcb0cdaff131932acbeaacead00cd925
Pierre Troin <pitroin@fu-c3-p4.s19.be>
2024-09-03 14:51:27 +0200

    readline

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pitroin <pitroin@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 12:00:19 by pitroin           #+#    #+#             */
-/*   Updated: 2024/09/03 12:01:11 by pitroin          ###   ########.fr       */
+/*   Updated: 2024/09/03 14:37:47 by pitroin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,13 @@
 # include <unistd.h>
 # include <limits.h>
 # include "../libft/libft.h"
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+typedef struct	s_shelly
+{
+	char	*cmd;
+	int		test;
+}	t_shelly;
 
 #endif
