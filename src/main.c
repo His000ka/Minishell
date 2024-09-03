f9e0001a169702c70b52f42a5a2de1315452f16d
Pierre Troin <pitroin@shi-r9-p4.s19.be>
2024-09-03 17:38:42 +0200

    fix

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pitroin <pitroin@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 12:02:29 by pitroin           #+#    #+#             */
-/*   Updated: 2024/09/03 16:37:09 by pitroin          ###   ########.fr       */
+/*   Updated: 2024/09/03 17:36:46 by pitroin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 int main(void)
 {
 	t_shelly	shelly;
-	while (1)
+	shelly.loop = 0;
+	while (shelly.loop == 0)
 	{
-		shelly.cmd = readline("bash> ");
+		shelly.cmd = readline("MINISHELL> ");
 		if (!shelly.cmd)
 			shelly.loop = ft_error("Error launching shell");
-		// printf("%s\n", shelly.cmd);
+		if(ft_strncmp(shelly.cmd, "exit", 4) == 0)
+			shelly.loop = ft_error("Exiting minishell\n");
 		add_history(shelly.cmd);
 		ft_parse(&shelly);
-		//shelly.loop = ft_error("Error launching shell");
 		free(shelly.cmd);
 	}
 }
