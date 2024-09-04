0cacd91075215346654dd6efcd38d4d4a0ba1259
Firdawsse Mazouz <fimazouz@mi-c2-p5.s19.be>
2024-09-04 15:46:25 +0200

    exit et echo

--- src/built-in/echo.c
+++ src/built-in/echo.c
@@ -6,7 +6,7 @@
 /*   By: fimazouz <fimazouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 15:46:35 by fimazouz          #+#    #+#             */
-/*   Updated: 2024/09/03 16:13:24 by fimazouz         ###   ########.fr       */
+/*   Updated: 2024/09/04 13:30:51 by fimazouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,38 @@
 
 int	if_echo(char **str)
 {
-	if(strncmp(str[0], "echo", 4) == 0)
-		return(1);
+	if (strncmp(str[1], "echo", 4) == 0)
+		return (1);
+	return (0);
 }
 
 void	ft_echo(char **str)
 {
-		
+	int	i;
+	int	option;
+	
+	i = 2;
+	option = 0;
+
+	if (str[i] && ft_strncmp(str[i], "-n", 2) == 0 && ft_strlen(str[i]) == 2)
+	{
+		option = 1;
+		i++;
+	}
+	while (str[i])
+	{
+		printf("%s", str[i]);
+		if (str[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (option == 0)
+		printf("\n");
 }
+
+// int	main(int ac, char **av)
+// {
+// 	if (if_echo(av))
+// 		ft_echo(av);
+// 	return (0);
+// }
