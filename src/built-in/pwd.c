a675dc8f9625ac58d43fd342aeb58bb26ce2d555
Firdawsse Mazouz <firdawssemazouz@MacBook-Air-de-Firdawsse.local>
2024-09-05 10:10:18 +0200

    add pwd

--- src/built-in/pwd.c
+++ src/built-in/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fimazouz <fimazouz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: firdawssemazouz <firdawssemazouz@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 14:38:31 by fimazouz          #+#    #+#             */
-/*   Updated: 2024/09/04 14:39:33 by fimazouz         ###   ########.fr       */
+/*   Updated: 2024/09/04 21:29:25 by firdawssema      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,38 @@
 
 int	if_pwd(char **str)
 {
-	if(strncmp(str[1], "pwd", 3) == 0)
+	if(strncmp(str[1], "pwd", 3) == 0 && (str[2] == NULL))
 		return(1);
 	return(0);
 }
 
+void	ft_pwd(void)
+{
+	char *cwd;
+	char *buffer;
+	size_t size;
+
+	size = 1024;
+	buffer = (char *)malloc(sizeof(char) * size);
+	if (!buffer)
+	{
+		perror("malloc");
+		return;
+	}
+	cwd = getcwd(buffer, size);
+	if (cwd)
+		printf("%s\n", cwd);
+	else
+		perror("getcwd");
+
+	free(buffer);
+}
+
+int	main(int ac, char **av)
+{
+	if (if_pwd(av))
+		ft_pwd();
+	else
+		printf("Commande inconnue : %s\n", av[1]);
+	return (0);
+}
