1291add7aa7a9952ec65b071decf7b972ea18716
Firdawsse Mazouz <firdawssemazouz@MacBook-Air-de-Firdawsse.local>
2024-09-17 11:07:56 +0200

    addin unset

--- src/built-in/unset.c
+++ src/built-in/unset.c
@@ -6,7 +6,7 @@
 /*   By: firdawssemazouz <firdawssemazouz@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 16:42:04 by fimazouz          #+#    #+#             */
-/*   Updated: 2024/09/15 19:22:00 by firdawssema      ###   ########.fr       */
+/*   Updated: 2024/09/17 11:07:13 by firdawssema      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,21 @@ int if_unset(char *str)
 	return (0);
 }
 
+// Helper function to check if a string is a valid identifier
+int is_valid_identifier(char *str)
+{
+	if (!str || (!ft_isalpha(str[0]) && str[0] != '_'))
+		return (0);
+	int i = 1;
+	while (str[i])
+	{
+		if (!ft_isalnum(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 // Function to remove a node from the environment list by its key
 void remove_env_node(t_env **env_list, char *key)
 {
@@ -29,7 +44,7 @@ void remove_env_node(t_env **env_list, char *key)
 	while (current != NULL)
 	{
 		// If the key matches, remove the node
-		if (ft_strcmp(current->content, key) == 0)
+		if (ft_strncmp(current->content, key, ft_strlen(key) + 1) == 0)
 		{
 			// Unlink the node from the list
 			if (previous == NULL)
@@ -49,7 +64,7 @@ void remove_env_node(t_env **env_list, char *key)
 }
 
 // Function to handle the 'unset' command
-int ft_unset(t_env **env_list, char **args)
+void ft_unset(t_env **env_list, char **args)
 {
 	int i = 1;
 
@@ -67,17 +82,16 @@ int ft_unset(t_env **env_list, char **args)
 		}
 		i++;
 	}
-	return (0);
 }
 
 // Uncomment the main for testing
-int main(int ac, char **av, char **envp)
-{
-    t_env *env_list = NULL;
-    env_list = create_env_list(&env_list, envp);
+// int main(int ac, char **av, char **envp)
+// {
+//     t_env *env_list = NULL;
+//     env_list = create_env_list(&env_list, envp);
 
-    if (if_unset(av[1]) == 1)
-        ft_unset(&env_list, av);
+//     if (if_unset(av[1]) == 1)
+//         ft_unset(&env_list, av);
 
-    return 0;
-}
+//     return 0;
+// }
